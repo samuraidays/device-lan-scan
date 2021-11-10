1ecb56e02620e03284c9f5c4c3b64e74c71eb456
takashi hasegawa <pocky@C02SM65PGTFJ.local>
2021-11-10 09:00:12 +0900

    first commit

---
+++ lan-arp-scan.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+NW="192.168.11"
+INTERFACE="en0"
+
+lsarp=`arp-scan -I ${INTERFACE} -l | grep ${NW} | sort -V | grep -v Interface`
+_res=`echo ${lsarp} | awk -v 'OFS=,' 'BEGIN {FS="\t"}{print $2,$3}' | sed -e "s/ /_/g" | sed -e "s/://g"` 
+array=($(echo $_res))
+
+thisdevice="`arp-scan -I ${INTERFACE} -l | grep Interface | awk '{print $6}' | sed -e"s/,//g" | sed -e "s/://g"`,`hostname`"
+array=("${array[@]}" ${thisdevice}) 
+
+#echo ${array[@]}
+for row in ${array[@]}
+do
+    echo "${row}"
+done
